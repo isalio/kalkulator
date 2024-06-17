8f5652536f7330b086d1c211716e4cff8e5ee82a
isalio <isalio144@gmail.com>
2024-06-17 16:31:23 +0300

    kalkulator

---
+++ kal.js
@@ -0,0 +1,41 @@
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input2');
+const plus = document.getElementById('plus');
+const minus = document.getElementById('minus');
+const del = document.getElementById('del');
+const umn = document.getElementById('umn');
+const ravno = document.getElementById('ravno');
+const otvet = document.getElementById('otvet');
+let action = '+';
+
+plus.onclick = function () {
+  action = '+';
+}
+
+minus.onclick = function () {
+  action = '-';
+}
+
+del.onclick = function () {
+  action = '/';
+}
+
+umn.onclick = function () {
+  action = '*';
+}
+
+ravno.onclick = function () {
+  if (action === '+') {
+    const sum = Number(input1.value) + Number(input2.value);
+    otvet.textContent = sum;
+  } else if (action === '-') {
+    const sum = Number(input1.value) - Number(input2.value);
+    otvet.textContent = sum;
+  } else if (action === '/') {
+    const sum = Number(input1.value) / Number(input2.value);
+    otvet.textContent = sum;
+  } else {
+    const sum = Number(input1.value) * Number(input2.value);
+    otvet.textContent = sum;
+  }
+}
